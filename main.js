43d094585aa86cf81633d244f61bfaf03a950342
Pabasara Jayasekera <108865623+pabasara-1120@users.noreply.github.com>
2024-11-11 23:26:39 +0530

    update backend url

--- main.js
+++ main.js
@@ -149,7 +149,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   const response = await fetch(
-    "http://localhost:https://intelligent-reverence-production.up.railway.app//api/chat",
+    "https://intelligent-reverence-production.up.railway.app/",
     {
       method: "POST",
       headers: {
